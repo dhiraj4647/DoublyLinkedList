de68815fd3219214a18724dacd16ea9187fd4c95
dhiraj4647 <84715353+dhiraj4647@users.noreply.github.com>
2021-08-07 17:27:17 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -7,20 +7,24 @@ for(i=0;i<n;i++){
       a[i]=rand()%50;
 }
 BubbleSort(a,n);
-for(i=0;i<n;i++){
+for(i=0;i<n;i++)
+{
       printf("%d\t",a[i]);
 }
 }
-void BubbleSort(int a[],int n){
+void BubbleSort(int a[],int n)
+{
 int i,j,temp;
 for(i=0;i<n-2;i++)
+  {
+      for(j=0;j<n-i-1;j++)
       {
-      for(j=0;j<n-i-1;j++){
-            if(a[j]>a[j+1]){
+            if(a[j]>a[j+1])
+            {
                   temp=a[j];
                   a[j]=a[j+1];
                   a[j+1]=temp;
             }
       }
-      }
+   }
 }
